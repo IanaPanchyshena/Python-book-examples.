72515637c71990196e43bbe9ac395f9aae9e9cbf
IanaPanchyshena <44506290+IanaPanchyshena@users.noreply.github.com>
2018-10-29 22:32:36 -0400

    Module 4

---
+++ module 4/simple_loop4-4.py
@@ -0,0 +1,9 @@
+#Simple_loop4_3
+
+# This program demonstrates a simple for loop
+# that uses a list of numbers.
+def main():
+    print('I will display the numbers 1 through 5.')
+    for num in [1, 2, 3, 4, 5]:
+        print(num)
+main()
