a3e2afa4f49f1e81a7076f2e77f08d5ff8fc5cc3
IanaPanchyshena <44506290+IanaPanchyshena@users.noreply.github.com>
2018-10-29 22:53:20 -0400

    Module 4

---
+++ module 4/simple_loop4_3.py
@@ -0,0 +1,9 @@
+# simple_loop4_3
+# This program also demonstrates a simple for
+# loop that uses a list of strings.
+
+def main():
+    for name in ['Winken','Blinken','Nod']:
+        print(name)
+main()
+
