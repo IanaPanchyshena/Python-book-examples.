7162262da375425b449a874029fca8e1e0765c42
IanaPanchyshena <44506290+IanaPanchyshena@users.noreply.github.com>
2018-10-29 22:55:32 -0400

    Module 4

---
+++ module 4/simple_loop4_2.py
@@ -0,0 +1,9 @@
+#simple_loop4_2
+# This program also demonstrates a simple for
+# loop that uses a list of numbers.
+
+def main():
+    print('I will display the odd numbers 1 through 9.')
+    for num in [1, 3, 5, 7, 9]:
+        print(num)
+main()
