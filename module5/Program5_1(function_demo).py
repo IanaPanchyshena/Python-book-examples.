0b745fb7263e3c6c39ff6afd9d88ea81632a4fb7
IanaPanchyshena <44506290+IanaPanchyshena@users.noreply.github.com>
2018-11-05 19:58:28 -0500

    Module5

---
+++ module5/Program5_1(function_demo).py
@@ -0,0 +1,14 @@
+# Program5_1(function_demo)
+
+# This program demonstrates a function.
+# First, we define a function named message.
+def main():
+    def message():
+        print('I am Arthur,')
+        print('King of the Britons.')
+
+
+# Call the message function.
+    message()
+
+main()
---
+++ module5/Program5_2(two_functions).py
@@ -0,0 +1,20 @@
+# Program5_2(two_functions)
+
+# This program demonstrates a function. First we
+# define the main function.
+def main():
+    
+    print('I have a message for you.')
+    message()
+    print('Goodbye!')
+
+# Next we define the message function.
+    def message():
+        print('I am Arthur,')
+        print('King of the Britons.')
+
+
+# Call the main function.
+
+main()
+
