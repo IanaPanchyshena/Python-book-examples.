64cc671be7352d782563a0c78ad3bf9afb3fb072
IanaPanchyshena <44506290+IanaPanchyshena@users.noreply.github.com>
2018-11-14 11:11:45 -0500

    Module 6

---
+++ module 6/Program6-6(write_numbers).py
@@ -0,0 +1,26 @@
+# Program6-6(write_numbers)
+
+# This program demonstrates how numbers
+# must be converted to strings before they
+# are written to a text file.
+
+def main():
+    # Open a file for writting.
+    outfile=open('numbers.txt','w')
+
+    # Get three numbers from user.
+    num1=int(input('Enter a number: '))
+    num2=int(input('Enter another number: '))
+    num3=int(input('Enter another number: '))
+
+    # Write the numbers to the file.
+    outfile.write(str(num1)+'\n')
+    outfile.write(str(num2)+'\n')
+    outfile.write(str(num3)+'\n')
+
+    # Close the file.
+    outfile.close()
+
+    # Close the main function.
+main()
+
