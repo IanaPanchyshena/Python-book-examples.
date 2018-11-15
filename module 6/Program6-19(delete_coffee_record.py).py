015b9af0e6e97e142de856ff18f82688607a6a33
IanaPanchyshena <44506290+IanaPanchyshena@users.noreply.github.com>
2018-11-14 23:17:12 -0500

    Module 6

---
+++ module 6/Program6-19(delete_coffee_record.py).py
@@ -0,0 +1,69 @@
+# Program6-19(delete_coffee_record.py)
+
+# This program allows the user to delete
+# a record in the coffee.txt file.
+
+import os # Needed for the remove and rename functions
+
+def main():
+    # Create a bool variable to use as a flag.
+    found=False
+
+    # Get the coffee to delete.
+    search=input('Which coffee do you want to delete? ')
+
+    # Open the original coffee.txt file
+    coffee_file=open('coffee.txt','r')
+
+    # Open the temporary file.
+    temp_file=open('temp.txt','w')
+
+    # Read the first record's description field.
+    descr=coffee_file.readline()
+
+    # Read the rest of the file.
+    while descr!='':
+        # Read the quantity field.
+        qty=float(coffee_file.readline())
+
+        # Strip the \n from the description.
+        descr=descr.rstrip('\n')
+
+        # If this is not the record to delete< then
+        # write it to the temporary file.
+        if descr !=search:
+            # Write the record to the temporary file.
+            temp_file.write(descr+'\n')
+            temp_file.write(str(qty)+'\n')
+        else:
+            # Set the found flag as True.
+            foind=True
+        # Read the next description.
+        descr=coffee_file.readline()
+
+    # Close the coffee file and the temporary file.
+    coffee_file.close()
+    temp_file.close()
+
+    # Delete the oroginal coffee.txt file.
+    os.remove('coffee.txt')
+
+    # Rename the temporary file.
+    os.rename('temp.txt','coffee.txt')
+
+    # If the search value was not found in the file display a message.
+    if found:
+        print('The file has been updated.')
+    else:
+        print('That item was not found in the file.')
+
+# Call the main funcion.
+main()
+
+        
+
+
+
+
+
+    
